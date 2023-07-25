d69f03a8b7a7ccda58d3c2534b82d6382fc7df4e
Jonathan Smoley <67881240+JT2M0L3Y@users.noreply.github.com>
2023-07-24 21:44:27 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,17 +5,22 @@ const path = require("path");
 async function main() {
   const readmeTemplate = (
     await fs.readFile(path.join(process.cwd(), "./README.template.md"))
-  ).toString("utf-8");
+  ).toString("utf-8")
+   .catch (error) { console.log("Read file failed"); };
   
   const quote_data = await (
     await fetch("https://zenquotes.io/api/random/")
-  ).json();
+       .catch (error) { console.log("Fetch failed"); };
+  ).json()
+   .catch (error) { console.log("Get data failed"); };
 
   const readme = readmeTemplate
     .replace("{quote}", quote_data[0].q)
     .replace("{author}", quote_data[0].a)
   
-  await fs.writeFile("README.md", readme);
+  await fs
+    .writeFile("README.md", readme)
+    .catch (error) { console.log("Write file failed"); };
 }
 
 main();
