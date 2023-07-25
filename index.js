d0c86892f0711153a93d79ba162bdc74b72a7d92
JT2M0L3Y <jtsmoley@icloud.com>
2023-07-25 15:54:04 -0700

    morph steps

--- index.js
+++ index.js
@@ -3,23 +3,21 @@ const { promises: fs } = require("fs");
 const path = require("path");
 
 async function main() {
-  try {
-    const readmeTemplate = (
-      await fs.readFile(path.join(process.cwd(), "./README.template.md"))
-    ).toString("utf-8");
-  
-    const quote_data = await (
-      await fetch("https://zenquotes.io/api/today/")
-    ).json();
+  const readmeTemplate = (
+    await fs.readFile(path.join(process.cwd(), "./README.template.md"))
+  ).toString("utf-8");
 
-    const readme = readmeTemplate
-      .replace("{quote}", quote_data[0].q)
-      .replace("{author}", quote_data[0].a)
-    
-    await fs.writeFile("README.md", readme);
-  } catch (error) {
-    alert(error.message);
-  }
+
+  const quote_data = await (
+    await fetch("https://zenquotes.io/api/today/")
+  ).json()
+    .catch((e) => { console.error(e.message); });
+
+  const readme = readmeTemplate
+    .replace("{quote}", quote_data[0].q)
+    .replace("{author}", quote_data[0].a)
+  
+  await fs.writeFile("README.md", readme);
 };
 
 main();
